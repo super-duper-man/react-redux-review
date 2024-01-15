2ec739c35442c960064b15fc2011476769140504
Danny <m.r.fouladi7@gmail.com>
2024-01-15 11:22:55 +0330

    redux pattern for counter slice initilized

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
-import useAsyncGenerator from './useAsyncGenerator'
-import { counter, handleRequests } from './utils/common-methods'
+import { decrement, increment } from './features/counter.slice';
 
 function App() {
-  const state = useAsyncGenerator<{data1: any, data2: any}>(handleRequests)
+  const counter = useSelector(state => state?.counter);
+  const dispatch = useDispatch();
   return (
-    <div>
-        <button onClick={state.refetch}>Refetch</button>
-        <div>
-          Requests Sent: {counter}
-        </div>
-        <pre>
-          <code>
-            {state.loading ? 'Loading...' : JSON.stringify(state.data, null, 2)}
-          </code>
-        </pre>
-
-        {state.error && <div>{state.error.message}</div>}
-      </div>
+    <>
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </>
   )
 }
 
